fc7c27324f89f2906717d0c78602664fda0be5ab
Диана Немиланова <dianatimosevskaya@MacBook-Air-Diana.local>
2025-09-02 10:06:35 +0300

    Task 1: optimize components with React.memo and PureComponent

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,53 @@
-import { Component } from 'react';
+import React, { Component, memo } from 'react';
 
 type IUser = {
-    name: string
-    age: number
-}
+    name: string;
+    age: number;
+};
 
 type IProps = {
-    user: IUser
-}
+    user: IUser;
+};
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
+const FirstComponent = memo(({ name, age }: IUser) => (
     <div>
         my name is {name}, my age is {age}
     </div>
-);
+));
 
-// functional component
-// Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
+// functional component with custom comparison (user object may change reference)
+const SecondComponent = memo(
+    ({ user: { name, age } }: IProps) => (
+        <div>
+            my name is {name}, my age is {age}
+        </div>
+    ),
+    (prevProps, nextProps) =>
+        prevProps.user.name === nextProps.user.name &&
+        prevProps.user.age === nextProps.user.age
 );
 
 // class component
-class ThirdComponent extends Component<IUser> {
+class ThirdComponent extends React.PureComponent<IUser> {
     render() {
         return (
             <div>
                 my name is {this.props.name}, my age is {this.props.age}
             </div>
-        )
+        );
     }
 }
 
 // class component
-class FourthComponent extends Component<IProps> {
+class FourthComponent extends React.PureComponent<IProps> {
     render() {
         return (
             <div>
                 my name is {this.props.user.name}, my age is {this.props.user.age}
             </div>
-        )
+        );
     }
 }
+
+export { FirstComponent, SecondComponent, ThirdComponent, FourthComponent };
